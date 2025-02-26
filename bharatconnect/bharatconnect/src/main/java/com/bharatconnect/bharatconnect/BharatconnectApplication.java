33cb7b11b66c191901cdb45bbcc83118c3a0e5f7
Sai Teja Rachamalla <87630632+saitejar110507@users.noreply.github.com>
2025-02-26 16:16:45 +0530

    Add files via upload

---
+++ bharatconnect/bharatconnect/src/main/java/com/bharatconnect/bharatconnect/BharatconnectApplication.java
@@ -0,0 +1,13 @@
+package com.bharatconnect.bharatconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BharatconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BharatconnectApplication.class, args);
+	}
+
+}
