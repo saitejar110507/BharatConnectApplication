ed3e9d28c960651d2c3dd00dda36beba34aaed0c
Sai Teja Rachamalla <87630632+saitejar110507@users.noreply.github.com>
2025-02-26 16:22:36 +0530

    Delete bharatconnect/bharatconnect directory

--- bharatconnect/bharatconnect/src/main/java/com/bharatconnect/bharatconnect/repository/Productrepository.java
+++
@@ -1,7 +0,0 @@
-package com.bharatconnect.repository;
-
-import com.bharatconnect.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
